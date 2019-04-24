5416d10b50e8581e4532c269cd3e826dd1dd097f
gleb <lfqdth5@gmail.com>
2019-04-24 22:41:35 +0300

    have updated build gradle

--- app/src/main/java/com/project/dajver/psypractice/App.java
+++ app/src/main/java/com/project/dajver/psypractice/App.java
@@ -4,8 +4,6 @@ import android.app.Application;
 import android.arch.persistence.room.Room;
 
 import com.project.dajver.psypractice.ui.favorite.db.DatabaseHelper;
-import com.crashlytics.android.Crashlytics;
-import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by gleb on 11/11/17.
@@ -23,7 +21,6 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        Fabric.with(this, new Crashlytics());
         instance = this;
         db = Room.databaseBuilder(getApplicationContext(), DatabaseHelper.class, "favorite-database")
                 .allowMainThreadQueries()
